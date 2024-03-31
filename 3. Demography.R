30a541187bf48d89679ad4647371ae94c2caef06
Lin <95630346+linyang1106@users.noreply.github.com>
2024-03-31 20:44:17 +0200

    Update 3. Demography.R

--- 3. Demography.R
+++ 3. Demography.R
@@ -19,10 +19,10 @@ metadata_FT2 <- data.frame(sample_data(ps_FT2))
 
 #baseline
 ################################### Table 1 fortifier
-myvar_all <- c("GA", "BW", "SGA","PMA", "delivery_mode", "multiplebirth_merge", "sex", "hospital_catch","region_catch", "probiotics_before")
+myvar_all <- c("GA", "BW", "SGA","PMA", "birth_mode", "multiplebirth_merge", "sex", "hospital", "probiotics_before")
 myvar_FT <- c("antibiotics","MMpercent3d")
-catvar <- c( "SGA", "delivery_mode", "multiplebirth_merge", "sex", "hospital_catch","region_catch", "antibiotics", "probiotics_before", "fortification")
-nonvar <- c("hospital_catch")
+catvar <- c( "SGA", "birth_mode", "multiplebirth_merge", "sex", "hospital", "antibiotics", "probiotics_before", "fortification")
+nonvar <- c("hospital")
 #basic characteristics
 tab_all <- CreateTableOne(vars = myvar_all, strata = "fortification", data = metadata_infant, factorVars = catvar)
 sepline_all <- print(tab_all, showAllLevels = TRUE, exact = nonvar,quote = TRUE, noSpaces = TRUE, printToggle = FALSE)
@@ -37,20 +37,20 @@ tab_FT2 <- CreateTableOne(vars = myvar_FT, strata = "fortification", data = meta
 sepline_FT2 <- print(tab_FT2, showAllLevels = TRUE, exact = nonvar,quote = TRUE, noSpaces = TRUE, printToggle = FALSE)
 
 #################################### Supplementary Table 1 birth mode
-myvar_all_BM <- c("GA", "BW", "SGA","PMA", "fortification", "multiplebirth_merge", "sex", "hospital_catch","region_catch", "probiotics_before")
-tab_all_BM <- CreateTableOne(vars = myvar_all_BM, strata = "delivery_mode", data = metadata_infant, factorVars = catvar)
+myvar_all_BM <- c("GA", "BW", "SGA","PMA", "fortification", "multiplebirth_merge", "sex", "hospital", "probiotics_before")
+tab_all_BM <- CreateTableOne(vars = myvar_all_BM, strata = "birth_mode", data = metadata_infant, factorVars = catvar)
 sepline_all_BM <- print(tab_all_BM, showAllLevels = TRUE, exact = nonvar,quote = TRUE, noSpaces = TRUE, printToggle = FALSE)
 #interventions
-tab_FT0_BM <- CreateTableOne(vars = myvar_FT, strata = "delivery_mode", data = metadata_FT0, factorVars = catvar)
+tab_FT0_BM <- CreateTableOne(vars = myvar_FT, strata = "birth_mode", data = metadata_FT0, factorVars = catvar)
 sepline_FT0_BM <- print(tab_FT0_BM, showAllLevels = TRUE, exact = nonvar,quote = TRUE, noSpaces = TRUE, printToggle = FALSE)
 
-tab_FT1_BM <- CreateTableOne(vars = myvar_FT, strata = "delivery_mode", data = metadata_FT1, factorVars = catvar)
+tab_FT1_BM <- CreateTableOne(vars = myvar_FT, strata = "birth_mode", data = metadata_FT1, factorVars = catvar)
 sepline_FT1_BM <- print(tab_FT1_BM, showAllLevels = TRUE, exact = nonvar,quote = TRUE, noSpaces = TRUE, printToggle = FALSE)
 
-tab_FT2_BM <- CreateTableOne(vars = myvar_FT, strata = "delivery_mode", data = metadata_FT2, factorVars = catvar)
+tab_FT2_BM <- CreateTableOne(vars = myvar_FT, strata = "birth_mode", data = metadata_FT2, factorVars = catvar)
 sepline_FT2_BM <- print(tab_FT2_BM, showAllLevels = TRUE, exact = nonvar,quote = TRUE, noSpaces = TRUE, printToggle = FALSE)
 
 ##################################### Supplementary Table 2 hospital and probiotics
-probiotics_infant <- select(metadata_rarefied, record_id, hospital_catch, probiotics_before)
+probiotics_infant <- select(metadata_rarefied, record_id, hospital, probiotics_before)
 probiotics_infant <- probiotics_infant[!duplicated(probiotics_infant$record_id),]
-table(probiotics_infant$hospital_catch,probiotics_infant$probiotics_before)
+table(probiotics_infant$hospital,probiotics_infant$probiotics_before)
